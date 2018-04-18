d6b990073bd4c9c95a38ff64cd0d1f9f25c2a4b6
Guest User <Guest@Epicodus-5D.local>
2018-04-18 14:25:08 -0700

    add scopes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,13 @@
 class List < ActiveRecord::Base
   has_many :tasks
+
+  scope :most_tasks, -> {(
+    select("lists.name, count(tasks.id) as tasks_counter")
+    .join(:tasks)
+    .group("lists.id")
+    .order("tasks_count DESC")
+    .limit(10)
+    )}
+    
   validates :name, :presence => true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :list
 
   validates :description, :presence => true
+
+  scope :today, -> { where("created_at >=?", Time.now.beginning_of_day) }
+  scope :ten_most_recent, -> { order(created_at: :desc).limit(10)}
 end
